fc5019f8d59831ed0f5e07be8033256090a83adf
Alallaf1 <parental.co122@gmail.com>
2024-04-11 12:27:54 +0300

    store the tasks in database and show form errors

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
@@ -22,20 +23,33 @@ Route::get('/', function() {
 
 Route::get('/tasks', function() {
     return view('index',[
-        'tasks' => \App\Models\Task::latest()->get()
+        'tasks' => Task::latest()->get()
     ]);
-})->name('tasks.index');
+})->name('/tasks.index');
 
-Route::view('tasks/create','create')
+Route::view('/tasks/create','create')
     ->name('tasks.create');
 
-Route::get('tasks/{id}', function($id) {
+Route::get('/tasks/{id}', function($id) {
     
     return view('show',[
-        'task'=> \App\Models\Task::findOrFail($id)
+        'task'=> Task::findOrFail($id)
     ]);
 })->name('tasks.show');
 
-Route::post('tasks',function(Request $request){
-    dd($request->all());
+Route::post('/tasks',function(Request $request){
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');;
 })->name('tasks.store');
